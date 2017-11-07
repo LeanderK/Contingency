dd73da3b29735769c08593bcc0d9f6b031eab53e
LeanderK <Leander.Kurscheidt@gmx.de>
2017-11-07 20:37:28 +0100

    Stuck on: No gradient defined for operation 'Svd_1' (op type: Svd). I bet this comes from the frechet-distance

--- contingency.py
+++ contingency.py
@@ -13,6 +13,7 @@ from numpy import random as nprandom
 # Adapted from https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/convolutional_network.py
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=False)
+#TODO max size?
 contingency = np.empty(shape=(784, 0))
 
 # Training Parameters
@@ -122,26 +123,32 @@ def withRandomContingency(features, labels, is_training):
 def withContingency(features, labels, is_training):
     (orig_loss_op, acc) = model_fn(features, labels, is_training, False)
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-    cont_batch = tf.get_variable(dtype=tf.float32, shape=[None, num_input], name="cont_batch")
-    cont_batch_la = tf.get_variable(dtype=tf.float32, shape=[None], name="cont_batch_labels")
+    num_adversarial = 10
+    cont_batch = tf.get_variable(dtype=tf.float32, shape=[batch_size + num_adversarial, num_input], name="cont_batch")
+    cont_batch_la = tf.get_variable(dtype=tf.float32, shape=[batch_size + num_adversarial], name="cont_batch_labels")
+
     #scalar that controls contingency 
-    cont_beta = tf.get_variable(dtype=tf.float32, shape=[], name="cont_batch")
+    cont_beta = tf.get_variable(dtype=tf.float32, shape=[], name="cont_beta")
 
     loss_with_cont = orig_loss_op + cont_beta * model_fn(cont_batch, cont_batch_la, is_training, True)
     train_op = optimizer.minimize(loss_with_cont,
                                 global_step=tf.train.get_global_step())
-    num_adversarial = 10
-    gen_images = tf.get_variable(dtype=tf.float32, shape=[num_adversarial, num_input], name="gen_images")
 
-    #there is a general myterioum surrounding this function. What does it do exactly? I have not get round 
+    #generating the contingengy
+    gen_images = tf.get_variable(dtype=tf.float32, shape=[num_adversarial, num_input], name="gen_images")
+    gen_labels = tf.get_variable(dtype=tf.float32, shape=[num_adversarial], name="gen_labels")
+
+    (cont_loss_op, cont_acc) = model_fn(gen_images, gen_labels, is_training, True)
+
+    #there is a general mysterium surrounding this function. What does it do exactly? I have not get round 
     #testing/investigating it yet.
     diff = tf.contrib.gan.eval.frechet_classifier_distance(
         features, 
         gen_images, 
         #I don't really understand what this is used for...
-        lambda imges : model_fn(imges, labels, is_training))
+        lambda imges : conv_net(imges, num_classes, False, is_training=is_training, should_reuse=True))
 
-    adversial_fitness = loss_op - diff
+    adversial_fitness = cont_loss_op - diff
     optimizer2 = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
     adv_op = optimizer2.minimize(-1 * adversial_fitness,
                                 global_step=tf.train.get_global_step(),
@@ -151,20 +158,28 @@ def withContingency(features, labels, is_training):
         (train_images, train_labels) = training
         (cont_img, cont_labels) = cont_training
 
+        # generates the congingency
         randomImages = nprandom.random((num_adversarial, num_input))
         zerolabels = np.zeros(num_adversarial)
         session.run(gen_images.assign(randomImages))
 
         for iteration in range(numGradAscentIter):
-            #TODO finish coding the minimizer, obtain calcuated values and pass them to the session below
+            #TODO does this work?
             a = session.run(adv_op, feed_dict={
-                    features.name: train_images,
-                    labels.name: train_labels,
+                    gen_images.name: train_images,
+                    gen_labels.name: train_labels,
                     is_training.name: False})
 
         adv_images = session.run(gen_images)
         cont_img = np.concatenate(cont_img, adv_images)
         cont_labels = np.concatenate(cont_labels, zerolabels)
+
+        #fill with random to archieve a static graph (is only relevant for the initialization)
+        to_fill = max(0, (batch_size + num_adversarial) - len(cont_img))
+        to_fill_images = nprandom.random((to_fill, num_input))
+        cont_img = np.concatenate(cont_img, randomImages)
+        to_fill_zerolabels = np.zeros(to_fill)
+        cont_labels = np.concatenate(cont_labels, to_fill_zerolabels)
 
         a, t = session.run([acc, train_op], feed_dict={
                 features.name: train_images,
